b63f569f3564d9137d13754e1a3b028c79a196da
Harshal Khadse <hkharshal28@gmail.com>
2025-02-20 17:17:19 +0530

    changes

---
+++ lib/db.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if(!MONGODB_URI){
+    throw new Error(" plese define mongodb uri in env file");
+}
+
+const cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = {conn: null, promise: null}
+}
+
+export async function connectToDatabase(){
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){ 
+        const opts = {
+        bufferCommands: true,
+        maxPoolSize: 10
+        };
+    
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection
+)}
+}
